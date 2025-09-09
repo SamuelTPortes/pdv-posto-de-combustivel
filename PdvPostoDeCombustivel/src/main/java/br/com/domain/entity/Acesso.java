84c77de85993e8f2cf4e643769f98b6cd2730d9f
Usuario <abc@gmail.com>
2025-09-09 20:14:30 -0300

    Recursos do dia 05-09 recuperados

---
+++ PdvPostoDeCombustivel/src/main/java/br/com/domain/entity/Acesso.java
@@ -0,0 +1,41 @@
+package br.com.domain.entity;
+
+public class Acesso {
+
+    // atributos
+
+    private String usuario;
+    private String senha;
+
+
+    // construtor
+
+    public Acesso(String usuario, String senha){
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+
+    // getters
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+
+    // setters
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+}
